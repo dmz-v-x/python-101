ca1c0823da0ec28df916e7ea11bfea54422bf599
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-02-04 15:12:17 +0530

    Create 0002-datatypes.py

---
+++ code/0002-datatypes.py
@@ -0,0 +1,34 @@
+# Integers
+
+a = 6
+b = -9
+print("Value of a is:", f"{a},", "value of b is:", b, sep=" ")
+
+# Float
+
+pi = 3.14
+temperature = 36.6
+print("Value of pi is:", f"{pi},", "value of temperature is:", temperature, sep=" ");
+
+# String
+
+message = "Hello World"
+code = "234josd9usdf"
+
+# Boolean
+
+is_active = True
+is_logged_in = False
+print(is_active)
+print(is_logged_in)
+
+# None
+
+result = None
+print(result)
+
+# Computing value while printing
+
+x = 10
+y = 5
+print("Sum of x and y is", x+y)
