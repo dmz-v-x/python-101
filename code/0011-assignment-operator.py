1786f39be5c86792d53e27837af72c40b60e5849
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-02-04 20:35:08 +0530

    Create 0011-assignment-operator.py

---
+++ code/0011-assignment-operator.py
@@ -0,0 +1,58 @@
+x = 10
+name = "Himanshu"
+is_active = True
+
+print(x) # 10
+print(name) # Himanshu
+print(is_active) # True
+
+x = 5
+x += 3
+print(x) # 8
+
+s = "Hello"
+s += "World"
+print(s) # Hello World
+
+x = 10
+x -= 4
+print(x) # 6
+
+x = 7
+x *= 5
+print(x) # 35
+
+msg = "Hi"
+msg *= 3
+print(msg) #HiHiHi
+
+x = 10
+x //= 4
+print(x) # 2
+
+x = 2
+x **= 3
+print(x) # 8
+
+x = 5
+y = 3
+x &= y
+print(x) # 1
+
+x = 5
+y = 3
+x |= y
+print(x) # 7
+
+x = 5
+y = 3
+x ^= y
+print(x) # 6
+
+x = 8
+x >>= 2
+print(x) # 2
+
+x = 3
+x <<= 2
+print(x) # 12
