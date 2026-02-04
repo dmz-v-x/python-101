1991b3f1e6fdddb3548c93d354dc6fc0a392bd0c
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-02-04 15:13:50 +0530

    Create 0003-type-function.py

---
+++ code/0003-type-function.py
@@ -0,0 +1,8 @@
+# type() function in Python returns the data type of a given object.
+
+print(type("True"))      # String
+print(type("Hello"))     # String
+print(type(2))           # Integer
+print(type(2.3))         # Float
+print(type(None))        # NoneType
+print(type(True))        # Boolean
