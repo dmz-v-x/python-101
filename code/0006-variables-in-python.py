02bd5c6dbe4f8ec96bf22f9bf90f47289a42259f
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-02-04 16:48:53 +0530

    Create 0006-variables-in-python.py

---
+++ code/0006-variables-in-python.py
@@ -0,0 +1,25 @@
+# Declaring a variable & assigning value to variable
+
+name = "Himanshu"
+age = 26
+language = "Python"
+
+print(name)
+print(age)
+print(language)
+
+# Rules for naming variable
+
+my_name = "Himanshu"   # ✅ Valid
+_age = 25              # ✅ Valid
+Name1 = "Python"       # ✅ Valid
+1name = "Error"        # ❌ Invalid (starts with number)
+for = "loop"           # ❌ Invalid (reserved keyword)
+
+# Multiple Assignment
+
+a, b, c = 5, 10, 15
+print(a, b, c)
+
+x = y = z = 100
+print(x, y, z)
