ac233185a0f66930e0411fb6f39a1582f16a460a
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-02-04 14:49:40 +0530

    Create 0001-printing-in-python.py

---
+++ code/0001-printing-in-python.py
@@ -0,0 +1,28 @@
+# Method 1: Using simple print function
+
+print("Hello World")
+
+# Method 2: Printing multiple values
+
+name = "Himanshu"
+age = 27
+
+print("Name:", name, "Age:", age)
+
+# Method 3: using sep parameter to separate multiple values
+
+print("python", "is", "easy", sep="-")
+print("python", "is", "easy", sep="_")
+
+# Method 4: Using end parameter
+
+print("Hello", end=" ")
+print("World")
+
+# Method 5: Using Formatted String
+
+print(f"Name: {name}, Age: {age}")
+
+# Method 6: Using str.format
+
+print("Name: {0}, Age: {1}".format(name, age))
