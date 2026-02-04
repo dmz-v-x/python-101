81ccd324da2c17c7b6421e94d79cf5284268c820
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-02-04 17:56:43 +0530

    Create 0010-arithmetic-operator.py

---
+++ code/0010-arithmetic-operator.py
@@ -0,0 +1,93 @@
+# Arithmetic Operator
+
+## Addition
+
+a = 5
+b = 3
+sum = a + b
+print(f"The sum of {a} and {b} is {sum}")
+
+x = 3
+y = 4.5
+sum = x + y
+print(f"The sum of {x} and {y} is {sum}")
+
+## String Concatenation
+
+str1 = "Hello"
+str2 = "World"
+print(str1 + str2)
+
+## Substraction
+
+a = 10
+b = 4
+result = a - b
+print(result)
+
+a = 10.5
+b = 5
+result = a - b
+print(result)
+
+## Multiplication
+
+a = 7
+b = 5
+result = a * b
+print(result)
+
+a = 7.5
+b = 3
+result = a * b
+print(result)
+
+print("Hello" * 3)
+
+## Division (/) -> True Division (Float)
+
+a = 10
+b = 4
+print(a/b) # 2.5
+
+print(5/0) # ZeroDivisionError
+
+## Floor Division (//)
+
+a = 10
+b = 4
+print(a // b) # 2
+
+## For negative numbers floor division is towards negative infinity
+
+print(-7 // 3) # -3
+
+## Modulo Operator (%)
+
+a = 10
+b = 4
+print(a % b) # 2
+
+## Exponentiation (**)
+
+a = 2
+b = 3
+print(a ** b) # 8
+
+print(9 ** 0.5) #3.0 (square root)
+
+## Operator precedence -> () > ** > * > / > // > % > + > -
+
+## Boolean with Numbers
+
+print(True + 3) # 4
+print(False * 10) # 0
+
+## Complex Numbers
+
+z = 1 + 2j
+print(z * 2) # (2 + 4j)
+print(type(z)) # <class 'complex'>
+
+
+
