af89654cdd1cbe564969e1f8bbbefdeda7795e9a
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-02-04 16:54:38 +0530

    Create 0008-dynamic-typing-in-python.py

---
+++ code/0008-dynamic-typing-in-python.py
@@ -0,0 +1,7 @@
+# Python allows us to change the data type of a variable during execution means we can re-assign value to same variable of different type
+
+x = 10
+print(type(x)) # int
+
+x = "Hello"
+print(type(x)) # str
