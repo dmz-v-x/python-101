c7c5524a3c17ff4289ce611b153cc8ca59044f31
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-02-04 15:15:34 +0530

    Create 0004-type-conversion.py

---
+++ code/0004-type-conversion.py
@@ -0,0 +1,5 @@
+num = "10"
+
+print(int(num))      # Converts string to integer → 10
+print(float(num))    # Converts string to float → 10.0
+print(str(20))       # Converts integer to string → "20"
