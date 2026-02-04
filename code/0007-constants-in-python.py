5a58d813b9438ee008a95bde42300f529a4a9d23
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-02-04 16:52:07 +0530

    Create 0007-constants-in-python.py

---
+++ code/0007-constants-in-python.py
@@ -0,0 +1,11 @@
+# Python doesn't have built-in way to declare constant like other languages, However the convention for declaring constants is that they are written in uppercase letters.
+
+PI = 3.14159
+MAX_USERS = 100
+APP_NAME = "My App"
+
+print(PI)
+print(MAX_USERS)
+print(APP_NAME)
+
+# Although we can re-assign a value to variable PI but not a recommended practice. 
